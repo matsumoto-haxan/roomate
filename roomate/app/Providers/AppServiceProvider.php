fe1759e5b182a945faebc521835b1a4b895afb3c
tomohax <matsumoto@haxan.co.jp>
2019-06-04 15:27:37 +0900

    DB接続・マイグレーションテストOK

--- roomate/app/Providers/AppServiceProvider.php
+++ roomate/app/Providers/AppServiceProvider.php
@@ -3,26 +3,28 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+  * Register any application services.
+  *
+  * @return void
+  */
+  public function register()
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+  /**
+  * Bootstrap any application services.
+  *
+  * @return void
+  */
+  public function boot()
+  {
+    // DBのString長を変更
+    Schema::defaultStringLength(191);
+  }
 }
